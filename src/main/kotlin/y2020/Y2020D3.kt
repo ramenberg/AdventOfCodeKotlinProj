a33cce50c60c779eda8ab4694d7c793116bba970
Emelie Ramenberg <ramenberg@gmail.com>
2023-02-17 13:46:04 +0100

    D3 pt 2

--- src/main/kotlin/y2020/Y2020D3.kt
+++ src/main/kotlin/y2020/Y2020D3.kt
@@ -1,6 +1,7 @@
 package y2020
 
 import java.io.File
+import java.math.BigInteger
 
 const val filePathD3 = "src/main/resources/inputY2020D3.txt"
 
@@ -18,6 +19,7 @@ fun readFileStringTo2DList(file: String): List<List<String>> {
     return charList
 }
 
+// del 1
 fun moveAndCountTrees(map: List<List<String>>): Int {
     var treeCounter = 0
     var x = 0
@@ -25,7 +27,6 @@ fun moveAndCountTrees(map: List<List<String>>): Int {
     while (y < map.size) {
         if (map[y][x] == "#") treeCounter++
         x = (x + 3) % map[0].size
-        println(x)
         y += 1
     }
     return treeCounter
@@ -44,7 +45,24 @@ fun moveAndCountTreesNo2(map: List<List<String>>, right: Int, down: Int): Int {
     return treeCounter
 }
 
+// del 2
+fun countAndMultiplyTrees(): Int {
+    val list = readFileStringTo2DList(filePathD3)
+    var counter = 1
+    val direction = listOf(Pair(1,1), Pair(3,1), Pair(5,1), Pair(7,1), Pair(1,2))
+
+    for (d in direction) {
+        counter *= moveAndCountTreesNo2(list, d.first, d.second)
+    }
+    return counter
+}
+
 fun main() {
     val list = readFileStringTo2DList(filePathD3)
+
+    // del 1
     println( moveAndCountTrees(list))
+
+    // del 2
+    println(countAndMultiplyTrees())
 }
--- src/test/kotlin/y2020/Y2020D3KtTest.kt
+++ src/test/kotlin/y2020/Y2020D3KtTest.kt
@@ -47,4 +47,94 @@ internal class Y2020D3KtTest {
         val resultList = readFileStringTo2DList(testFilePath)
         assertEquals(expectedSize, resultList.size)
     }
+
+    @Test
+    fun moveAndCountTreesShouldBeEquals() {
+        val expectedValue = 1
+        val testFilePath = "src/test/resources/testInput.txt"
+        val testFile = File(testFilePath)
+        testFile.writeText(
+            "...#.\n" +
+                    "...#.\n" +
+                    "..#..\n" +
+                    "...#.\n" +
+                    "...#.\n" +
+                    "..#.."
+        )
+        val resultList = readFileStringTo2DList(testFilePath)
+        val result = moveAndCountTrees(resultList)
+        assertEquals(expectedValue, result)
+    }
+    @Test
+    fun moveAndCountTreesShouldNotBeEquals() {
+        val expectedValue = 6 // The amount times NO. is written to the test file.
+        val testFilePath = "src/test/resources/testInput.txt"
+        val testFile = File(testFilePath)
+        testFile.writeText(
+            "...#.\n" +
+                    "...#.\n" +
+                    "..#..\n" +
+                    "...#.\n" +
+                    "...#.\n" +
+                    "..#.."
+        )
+        val resultList = readFileStringTo2DList(testFilePath)
+        val result = moveAndCountTrees(resultList)
+        assertNotEquals(expectedValue, result)
+    }
+
+    @Test
+    fun moveAndCountTreesNo2Pair1() {
+        val expectedValue = 2
+        val testFilePath = "src/test/resources/testInput.txt"
+        val pairs = listOf(Pair(1,1))
+        val testFile = File(testFilePath)
+        testFile.writeText(
+            "...#.\n" +
+                    "...#.\n" +
+                    "..#..\n" +
+                    "...#.\n" +
+                    "...#.\n" +
+                    "..#.."
+        )
+        val resultList = readFileStringTo2DList(testFilePath)
+        val result = moveAndCountTreesNo2(resultList, pairs[0].first, pairs[0].second)
+        assertEquals(expectedValue, result)
+    }
+    @Test
+    fun moveAndCountTreesNo2Pair2() {
+        val expectedValue = 0
+        val testFilePath = "src/test/resources/testInput.txt"
+        val pairs = listOf(Pair(1,2))
+        val testFile = File(testFilePath)
+        testFile.writeText(
+            "...#.\n" +
+                    "...#.\n" +
+                    "..#..\n" +
+                    "...#.\n" +
+                    "...#.\n" +
+                    "..#.."
+        )
+        val resultList = readFileStringTo2DList(testFilePath)
+        val result = moveAndCountTreesNo2(resultList, pairs[0].first, pairs[0].second)
+        assertEquals(expectedValue, result)
+    }
+
+//    @Test
+//    fun countAndMultiplyTrees() {
+//        val pairs = listOf(Pair(3,1), Pair(5,1), Pair(1,2))
+//        val expectedValue = 336
+//        val testFilePath = "src/test/resources/testInput.txt"
+//        val testFile = File(testFilePath)
+//        testFile.writeText(
+//            "...#.\n" +
+//                    "...#.\n" +
+//                    "..#..\n" +
+//                    "...#.\n" +
+//                    "...#.\n" +
+//                    "..#.."
+//        )
+//        val resultList = readFileStringTo2DList(testFilePath)
+//        val result = countAndMultiplyTrees(resultList, pairs)
+//    }
 }
